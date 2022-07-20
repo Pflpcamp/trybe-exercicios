5394109b24f18d0a3189f3c81dd28842c0b94d51
Paulo Felipe Campelo <pflpcampelo@gmail.com>
2022-07-20 14:56:54 -0300

    ate exercicio 5

---
+++ 01-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/01b-javascript-primeiro-passos/ex3.js
@@ -0,0 +1,15 @@
+/*Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.*/
+
+let a = 210
+let b = 102
+let c = 892
+
+if(a > b && a > c){
+    console.log("A é o maior número")
+}
+else if(b > a && b > c){
+    console.log("B é o maior número")
+}
+else {
+    console.log("C é o maior número")
+}
---
+++ 01-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/01b-javascript-primeiro-passos/ex4.js
@@ -0,0 +1,13 @@
+/*Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.*/
+
+const x = -21;
+
+if(x < 0){
+    console.log('negative')
+}
+else if(x > 0){
+    console.log('positive')
+}
+else {
+    console.log('negative')
+}
