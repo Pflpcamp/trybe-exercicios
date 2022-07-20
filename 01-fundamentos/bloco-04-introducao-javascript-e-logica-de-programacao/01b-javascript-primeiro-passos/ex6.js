bdf4ff4668737043ea3ffa15483d6cadbc8bfe34
Paulo Felipe Campelo <pflpcampelo@gmail.com>
2022-07-20 15:30:47 -0300

    ate exercicio 7

---
+++ 01-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/01b-javascript-primeiro-passos/ex6.js
@@ -0,0 +1,7 @@
+/*Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.*/
+
+const peca = "bISHOP";
+
+if(peca == "bishop" || peca.toLowerCase() == "bishop"){
+    console.log("diagonais")
+}
---
+++ 01-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/01b-javascript-primeiro-passos/ex7.js
@@ -0,0 +1,28 @@
+/*screva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+Porcentagem >= 90 -> A
+Porcentagem >= 80 -> B
+Porcentagem >= 70 -> C
+Porcentagem >= 60 -> D
+Porcentagem >= 50 -> E
+Porcentagem < 50 -> F */
+
+let nota = 45
+
+if(nota >= 90){
+    console.log("Nota A")
+}
+else if(nota >= 80 && nota < 90){
+    console.log("Nota B")
+}
+else if(nota >= 70 && nota < 80){
+    console.log("Nota C")
+}
+else if(nota >= 60 && nota < 70){
+    console.log("Nota D")
+}
+else if(nota >=50 && nota < 60){
+    console.log("Nota E")
+}
+else {
+    console.log("Nota F")
+}
