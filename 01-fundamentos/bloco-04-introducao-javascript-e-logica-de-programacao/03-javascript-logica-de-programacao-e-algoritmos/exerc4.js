cee55b5fb197e66412ea9282960c61134da62677
Paulo Felipe Campelo <pflpcampelo@gmail.com>
2022-07-22 20:36:11 -0300

    mudança no exercicio dia 4.3

--- 01-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/03-javascript-logica-de-programacao-e-algoritmos/exerc4.js
+++ 01-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/03-javascript-logica-de-programacao-e-algoritmos/exerc4.js
@@ -1,6 +1,7 @@
 /*Um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 2 e 50.*/
 let primo = 0;
 let divisores = 0;
+let ultimoPrimo
 for(i = 2;i <= 50;i++){
     for(j = 1; j <= i;j++){
         if(i%j == 0){
@@ -9,10 +10,11 @@ for(i = 2;i <= 50;i++){
     }
     if(divisores <= 2){
         primo++;
+        ultimoPrimo = i;
     }
     divisores = 0; 
 }
 
-console.log(primo);
+console.log('Existem ' + primo + ' números primos entre esses dois números. O último número primo é', ultimoPrimo);
 
 
