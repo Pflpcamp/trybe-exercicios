93c3243e2e0a72c950a577d5e15fab7a332ca49a
Paulo Felipe Campelo <pflpcampelo@gmail.com>
2022-07-27 15:10:07 -0300

    começando exercicio

---
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/01-javascript-dom-e-seletores/exercicio1/script.js
@@ -0,0 +1,11 @@
+document.getElementById("header-container").style.backgroundColor = "#40c080"
+
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "#fd8e72"
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "#f7b655"
+
+document.getElementsByTagName("h3")[0].style.backgroundColor = "#cc33ff"
+document.getElementsByTagName("h3")[1].style.backgroundColor = "#cc33ff"
+document.getElementsByTagName("h3")[2].style.backgroundColor = "#000000"
+document.getElementsByTagName("h3")[3].style.backgroundColor = "#000000"
+
+document.getElementById("footer-container").style.backgroundColor = "#133a26"
