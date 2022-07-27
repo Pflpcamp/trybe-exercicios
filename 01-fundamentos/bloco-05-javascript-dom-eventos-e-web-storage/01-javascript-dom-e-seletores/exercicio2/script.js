0bd5a1ea0351437c48e8cd7de0e415686634b976
Paulo Felipe Campelo <pflpcampelo@gmail.com>
2022-07-27 16:00:01 -0300

    exercicios do dia

---
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/01-javascript-dom-e-seletores/exercicio2/script.js
@@ -0,0 +1,21 @@
+
+function mudeTexto() {
+    return document.getElementsByTagName("p")[1].innerText = 'Daqui a dois anos me vejo trabalhando como desenvolvedor de software fullstack.'}
+
+function mudeCor() {
+    return document.getElementsByClassName("main-content")[0].style.backgroundColor = 'rgb(76,164,109)'}
+
+function mudeCorCentro() {
+    return document.getElementsByClassName("center-content")[0].style.backgroundColor = '#ffffff';}
+
+function corrigeTexto() {
+    return document.getElementsByTagName("h1")[0].innerText = 'Exercício 5.1 - JavaScript';}
+
+function mostraTexto() {
+    return console.log(document.getElementsByTagName("p")[0].innerText +'\n'+ document.getElementsByTagName("p")[1].innerText + '\n' + document.getElementsByTagName("p")[2].innerText);
+    }
+
+
+
+
+
